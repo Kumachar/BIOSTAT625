6358fdece0a0390ba431ac8798e48034c7cdc999
Kumachar <1019190564@qq.com>
2023-11-10 10:08:23 -0500

    newchanges

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,12 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' Add together two numbers
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return A number.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
 
-hello <- function() {
-  print("Hello, world!")
+add <- function(x, y) {
+  x + y
 }
